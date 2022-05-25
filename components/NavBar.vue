b800bd6a175ab208ceaf1bf1bd3e11ad86392879
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-05-25 20:31:31 +0200

    added part of navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,56 +1,26 @@
 <template>
-  <v-card
-    class="mx-auto overflow-hidden"
-    height="400"
-  >
-    <v-app-bar
-      color="red"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-5"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+  <v-app>
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>WEATHER WETTEN</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>PROFILE</v-btn>
+        <v-btn flat>BET</v-btn>
+        <v-btn flat>LEADERBOARDS</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
-}
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
 </script>
-<style scoped>
-</style>
+
+<style></style>

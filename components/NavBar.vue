bf4110f06d1e4f8e4aa48faf54f270a29c03063e
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-05-25 21:06:37 +0200

    added one nuxlink (with design problems)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,26 +1,50 @@
 <template>
   <v-app>
-    <v-toolbar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>WEATHER WETTEN</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>PROFILE</v-btn>
-        <v-btn flat>BET</v-btn>
-        <v-btn flat>LEADERBOARDS</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app color="black">
+      <v-toolbar color="black">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>WEATHER WETTEN</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn color="black" flat><nuxt-link to="/profile" tag="a">PROFILE</nuxt-link></v-btn>
+          <v-btn color="black" flat>BET</v-btn>
+          <v-btn color="black"  flat>LEADERBOARDS</v-btn>
+          <v-btn color="black" flat>WEATHERCOIN</v-btn>
+          <v-btn  color="black" flat>ABOUT US</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-  methods: {
-    toggleTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-    },
-  },
-};
+export default {}
 </script>
 
-<style></style>
+<style>
+.nuxt-link-active {
+  font-weight: bold;
+}
+.nuxt-link-active {
+  color: red;
+}
+</style>

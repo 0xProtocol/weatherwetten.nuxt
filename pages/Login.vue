3a988b0695f2162c1ad2e4d4d05e6c429f03dfb3
Timothy Nicholson <timmy.nicholson@hotmail.com>
2022-05-25 21:15:12 +0200

    added basic layout for login page

---
+++ pages/Login.vue
@@ -0,0 +1,65 @@
+<template>
+<!--<v-app>-->
+  <v-content>
+    <v-card  width="550" id="login">
+      <v-card-title>Login to WeatherWetten!</v-card-title>
+      <v-card-text>
+        <v-text-field label="E-Mail" />
+        <v-text-field
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"/>
+      </v-card-text>
+
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="blue">Login</v-btn>
+        <v-btn color="green">New here?</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-content>
+<!--</v-app>-->
+
+
+
+
+</template>
+
+
+<script>
+import NavBar from "@/components/NavBar";
+export default {
+  data(){
+    return{
+      showPassword:false
+    }
+  },
+
+  name: "Login",
+
+  components: {
+    NavBar
+
+  }
+}
+</script>
+
+<style scoped>
+
+
+#login{
+  position: fixed;
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+}
+
+
+
+
+
+
+
+</style>

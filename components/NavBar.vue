520f3be340da83ca199336991dc6f6a48eab7d80
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-05-26 22:17:59 +0200

    added text over hero section (actually problems with v-overlay)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -60,15 +60,25 @@
     </v-navigation-drawer>
 
     <v-main color="white">
+
       <span class="hero"></span>
-            <!-- <iframe
-              width="560"
-              height="315"
-              :src="require('../assets/img/video.mp4')"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe> -->
+      <!-- Overlay over img-->
+      <v-overlay absolute>
+        <v-card flat color="transparent" >
+          <v-card-text class="white--text font-weight-black text-center d-flex flex-column align-center justify-center"
+            :class="headingClass">BET WITH WEATHERCOIN</v-card-text>
+          <v-card-subtitle class="white--text text-center d-flex flex-column align-center justify-center" :class="subtitleClass">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem blanditiis, commodi cum dignissimos dolor dolore enim eum id illum in iusto, nemo omnis placeat, quis quisquam<br> sed sequi veritatis. Minima, quibusdam, quos! Consectetur ducimus eaque et, fuga iure labore maiores modi <br>possimus praesentium quas quisquam quod quos ratione, reiciendis!</v-card-subtitle>
+        </v-card>
+      </v-overlay>
+
+      <!-- <iframe
+        width="560"
+        height="315"
+        :src="require('../assets/img/video.mp4')"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> -->
     </v-main>
 
     <v-footer app>
@@ -105,5 +115,10 @@ export default {
   top: 0;
   left: 0;
   transform: scale(1.03); /*to scale img right to main element (not really a good way but working)*/
+}
+.overlay{
+  justify-content: center;
+  align-items: center;
+  transform: scale(1.03);
 }
 </style>

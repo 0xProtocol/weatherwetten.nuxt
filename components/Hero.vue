1bb7b74b9629875d18f50f687335b3c5f427ee99
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-05-31 12:44:09 +0200

    changed from img to vid and changed text pos a little bit

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,11 +2,13 @@
 <v-app>
 <v-main color="white">
 
-<span class="hero"></span>
+<!-- <span class="hero"></span> -->
+  <video class ="herovid" playsinline autoplay muted loop>
+    <source :src='require("../assets/img/video.mp4")' type='video/mp4'>
+  </video>
 <!-- Overlay over img-->
   <v-card flat color="transparent" >
-    <v-card-text class="white--text font-weight-black text-center d-flex flex-column align-center justify-center"
-                 :class="headingClass">BET WITH WEATHERCOIN</v-card-text>
+    <v-card-text class="white--text font-weight-black text-center d-flex flex-column align-center justify-center headingClass">BET WITH WEATHERCOIN</v-card-text>
     <v-card-subtitle class="white--text text-center d-flex flex-column align-center justify-center" :class="subtitleClass">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem blanditiis, commodi cum dignissimos dolor dolore enim eum id illum in iusto, nemo omnis placeat, quis quisquam<br> sed sequi veritatis. Minima, quibusdam, quos! Consectetur ducimus eaque et, fuga iure labore maiores modi <br>possimus praesentium quas quisquam quod quos ratione, reiciendis!</v-card-subtitle>
     <v-container class="fill-height align-center justify-center">
       <v-btn  class="white--text font-weight-black text-center d-flex flex-column align-center justify-center">
@@ -30,7 +32,7 @@
 
 <style>
 .hero {
-  background: url('../assets/img/hero.jpg');
+  background: url('../assets/img/video.mp4');
   background-size: cover;
   width: 100%;
   height: 100%;
@@ -38,6 +40,11 @@
   top: 0;
   left: 0;
   transform: scale(1.03); /*to scale img right to main element (not really a good way but working)*/
+}
+.herovid {
+  position: absolute;
+  top: -28%; /* from browser to browser different  */
+
 }
 .nuxt-link-active {
   font-weight: bold;
@@ -51,6 +58,12 @@
   font-weight: bold;
   color: green;
   text-decoration: none;
+}
+.headingClass
+{
+  font-size: 25px;
+  margin-top: 250px;
+  color: blue;
 }
 </style>
 

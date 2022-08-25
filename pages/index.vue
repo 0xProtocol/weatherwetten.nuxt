307730971c6afe99b74c484403931db5c763199a
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-08-25 21:17:24 +0200

    added new pictures into the preloader

--- pages/index.vue
+++ pages/index.vue
@@ -113,10 +113,12 @@ html{
   url('../assets/img/benefit3.png'), url('../assets/img/betting.jpg'), url('../assets/img/cards.jpg'),
   url('../assets/img/climate.png'), url('../assets/img/cold.jpg'), url('../assets/img/coldcloud.jpg'), url('../assets/img/colddrizzle.jpg'),
   url('../assets/img/coldrain.jpg'), url('../assets/img/coldsnow.jpg'), url('../assets/img/coldthunderstorm.jpg'), url('../assets/img/concept.png'),
-  url('../assets/img/data.png'), url('../assets/img/fast.png'), url('../assets/img/leaderboards.png'), url('../assets/img/leaderboards4.jpg'),
+  url('../assets/img/data.png'), url('../assets/img/fast.png'), url('../assets/img/leaderboards4.jpg'),
   url('../assets/img/login.png'), url('../assets/img/secure.png'), url('../assets/img/sunny.jpg'), url('../assets/img/warm.jpg'),
   url('../assets/img/warmclouds.png'), url('../assets/img/warmdrizzle.jpg'), url('../assets/img/warmrain.jpg'), url('../assets/img/warmsnow.jpg'),
-  url('../assets/img/warmthunderstorm.jpg'), url('../assets/img/weathercoin.gif'), url('../assets/img/weatherstation2.png'), url("../assets/img/betting2.jpg");
+  url('../assets/img/warmthunderstorm.jpg'), url('../assets/img/weathercoin.gif'), url("../assets/img/betting2.jpg"),
+  url("../assets/img/aboutus1.png"),url("../assets/img/aboutus2.png"),url("../assets/img/aboutus3.png"),url("../assets/img/aboutus4.png"),url("../assets/img/weathercoin2.gif"),
+  ,url("../assets/img/profile3.jpg") ,url("../assets/img/concept.jpg");
 
 
   background-size: 0;

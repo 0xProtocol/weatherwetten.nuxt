8bdd552be039286da6e0d6b100ee23d48ba55edb
Konstant-Coding <91529510+Konstant-Coding@users.noreply.github.com>
2022-08-29 11:06:56 +0200

    Added BetClass with constructor

---
+++ js/BetClass.js
@@ -0,0 +1,14 @@
+
+class Bet {
+
+  constructor(bettedValue, bettedCoins, time, betType) {
+    this.bettedValue = bettedValue;
+    this.bettedCoins = bettedCoins;
+    this.time = time;
+    this.betType = betType;
+  }
+
+
+
+
+}

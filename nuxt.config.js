31f0f0851fee1a0a0851f8fe32b0fae850120a29
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-09-02 14:09:57 +0200

    changed to better title name for screen reader

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - weatherwetten.nuxt',
-    title: 'weatherwetten.nuxt',
+    titleTemplate: '',
+    title: 'Weather Wetten',
     htmlAttrs: {
       lang: 'en'
     },
